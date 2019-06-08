0dc29f44bb41dc97eb4c8c7621e4eb64cc4466cf
Craig Warren <craig@craigwarren.co.uk>
2019-06-08 17:04:36 +0100

    Adding a version of wordcount using datasets

---
+++ src/main/java/example/DatasetWordCount.java
@@ -0,0 +1,22 @@
+package example;
+
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.sql.*;
+
+import java.util.Arrays;
+
+public class DatasetWordCount {
+
+    public static void main(String[] args) {
+        SparkSession spark = SparkSession.builder().appName("Dataset Word Count").getOrCreate();
+        Dataset<String> strings = spark.read().textFile(args[0]);
+        Dataset<String> words = strings.flatMap(
+                (FlatMapFunction<String, String>) x -> Arrays.asList(x.split(" ")).iterator(),
+                Encoders.STRING()
+        );
+        Dataset<Row> wordCount = words.groupBy("value").count().toDF("word", "count");
+        Dataset<Row> orderedWordCount = wordCount.orderBy(functions.desc("count"));
+        orderedWordCount.write().csv(args[1]);
+    }
+
+}
